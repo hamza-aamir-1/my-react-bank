44ee02e2f03109abe211ef005aa9903bb3d77428
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-05-22 00:17:32 +0500

    Navbar Styling Done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,22 +4,25 @@ import './Navbar.css'
 const Navbar = () => {
   return (
 
-<nav className="navbar navbar-expand-lg bg-primary bg-gradient container">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">My Bank</a>
+<nav className="navbar navbar-expand-lg bg-primary bg-gradient p-0">
+  <div className="container">
+    <a className="navbar-brand text-white d-flex align-items-center" href="#">
+        <img src="favicon.png" className='me-2' alt="" />
+        My Bank
+    </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link" aria-current="page" href="#">Dashboard</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">Accounts</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Disabled</a>
+          <a className="nav-link" href="#">Transactions</a>
         </li>
       </ul>
     </div>
