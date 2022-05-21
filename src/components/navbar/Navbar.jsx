e7fa52146c08e51ef1c7a7385ee8934dd55bb404
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-05-21 23:58:07 +0500

    Navbar Added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,27 @@ import './Navbar.css'
 const Navbar = () => {
   return (
 
-
+<nav className="navbar navbar-expand-lg bg-primary bg-gradient container">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">My Bank</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
   )
 }
